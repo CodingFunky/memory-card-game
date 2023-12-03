a3b949e5cee7f125bed50628f0fea863d9556736
CodingFunky <ryan.namingishard2@gmail.com>
2023-12-03 16:38:38 -0700

    fetching data and showing cards correctly

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,89 @@ import Card from "./components/card.jsx";
 import "./App.css";
 
 function App() {
+  const [allCharacters, setAllCharacters] = React.useState({});
+  const [mode, setMode] = useState(3);
+  const [characters, setCharacters] = React.useState([]);
+  const [score, setScore] = React.useState(0);
+  const [clickedCharacters, setClickedCharacters] = React.useState([]);
+  const [result, setResult] = React.useState("");
+
+  function getCharacters() {
+    fetch(`https://dragonball-api.com/api/characters?page=1&limit=300`)
+      .then((res) => res.json())
+      .then((data) => {
+        setAllCharacters(data.items);
+        let newCharacters = [];
+        for (let i = 0; i < mode; i++) {
+          let randomID = Math.floor(Math.random() * 57) + 1;
+          newCharacters.push(data.items[randomID]);
+        }
+        setCharacters(newCharacters);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }
+
+  //To get the characters on load
+  React.useEffect(() => {
+    getCharacters();
+  }, []);
+
+  //Resets game
+  function reset() {
+    setCharacters([]);
+    setScore(0);
+    setClickedCharacters([]);
+    getCharacters();
+  }
+
+  function shuffleCharacters() {
+    let newOrder = characters;
+    for (let i = newOrder.length - 1; i > 0; i--) {
+      // Generate a random index lower than the current element
+      const j = Math.floor(Math.random() * (i + 1));
+      // Swap elements at indices i and j
+      [newOrder[i], newOrder[j]] = [newOrder[j], newOrder[i]];
+    }
+  }
+
+  function handleClick(id) {
+    setResult(" ");
+    console.log("clicked");
+    if (clickedCharacters.includes(id)) {
+      // game over/reset game
+      setResult("You lose!");
+      reset();
+    } else {
+      //update list or win game
+      setClickedCharacters((prev) => [...prev, id]);
+      setScore((prev) => prev + 1);
+      if (score === mode - 1) {
+        console.log("You win!");
+        setResult("Winner!");
+        reset();
+      }
+    }
+    shuffleCharacters();
+  }
+
   return (
     <>
       <h1 className="title">Memory Game</h1>
       <p className="rules">
         Click all pictures without clicking the same one twice!
       </p>
-      <p className="scoreBoard">Score: </p>
+      <p className="scoreBoard">Score: {score}</p>
       <div className="card-container">
-        <Card />
-        <Card />
-        <Card />
+        <Card characters={characters} id={0} onClick={handleClick} />
+        <Card characters={characters} id={1} onClick={handleClick} />
+        <Card characters={characters} id={2} onClick={handleClick} />
       </div>
+      <button className="reset" onClick={reset}>
+        Get New character
+      </button>
+      <div className="results">{result && <p>{result}</p>}</div>
     </>
   );
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,29 +1,16 @@
 import React from "react";
 
-// async function getRandomPokemon() {
-//   const randomID = Math.floor(math.random() * 10) + 1;
-
-//   const response = await fetch(
-//     `https://pokeapi.co/api/v2/pokemon/${randomId}/`
-//   );
-//   console.log("render");
-//   const data = await response.json();
-
-//   return { name: data.name, image: data.sprites.front_default };
-// }
-
-function Card() {
-  const randomID = Math.floor(Math.random() * 100) + 1;
-  const [pokemon, setPokemon] = React.useState([]);
-
-  React.useEffect(() => {
-    console.log("fetch");
-    fetch(`https://pokeapi.co/api/v2/pokemon-form/${randomID}/`)
-      .then((res) => res.json())
-      .then((data) => setPokemon(data.sprites.front_default));
-  }, []);
-  // console.log(pokemon.sprites.front_default);
-  return <div className="card">{<img src={pokemon} alt="" />}</div>;
+function Card(props) {
+  console.log(props.characters);
+  return (
+    <div className="card" onClick={() => props.onClick(props.id)}>
+      <img
+        className="card-image"
+        src={props.characters[props.id]?.image}
+        alt=""
+      />
+    </div>
+  );
 }
 
 export default Card;
